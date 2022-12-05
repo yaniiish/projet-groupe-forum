2e7b0f0b5732dc51e20a6ddef407a3cdd47aa3eb
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2022-12-05 19:05:13 +0100

    Accueil
    footer + Accueil

--- accueil.php
+++ accueil.php
@@ -1,4 +1,45 @@
-<?php
-include 'header.php';
-echo 'Hello World !';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <?php
+        include 'header.php';
+    ?>
+
+    <section id="bienvenue">
+        <h1>Bienvenue dans le Forum des Dev'</h1>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Tempore nulla nesciunt perspiciatis. 
+            Corrupti aperiam veritatis necessitatibus optio fugit 
+            numquam vitae at recusandae, cupiditate maxime porro 
+            harum facilis vel quam culpa! Tenetur, 
+            iure adipisci cum est reiciendis quo dignissimos 
+            dolores voluptas ab eius fugit perferendis minus corporis 
+            ex numquam cumque dolor nostrum maxime voluptatibus sequi 
+            quia! Nulla minima autem iure iusto architecto consectetur 
+            magni sit fuga debitis. Consectetur eligendi cumque cum 
+            facilis rerum velit voluptate molestias nobis delectus modi 
+            ipsam autem blanditiis, ab vel dolor id nulla sit voluptates 
+            explicabo nostrum temporibus. Quas nihil nesciunt consectetur, 
+            veritatis provident culpa aspernatur nemo.
+        </div>
+    </section>
+
+
+    
+    <?php
+        include 'footer.php';
+    ?>
+
+</body>
+</html>
+
