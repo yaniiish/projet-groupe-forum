060e5dc8ba391d2ce338b028467848c93106c133
samantha-teixeira <samerika69@gmail.com>
2022-12-05 15:59:18 +0100

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,3 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 05 déc. 2022 à 14:58
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `forum_dev`
+--
+CREATE DATABASE IF NOT EXISTS `forum_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `forum_dev`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+DROP TABLE IF EXISTS `commentaires`;
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `contenu` varchar(4096) DEFAULT NULL,
   `pseudo` varchar(256) DEFAULT NULL,
   `id_sujet` int(11) DEFAULT NULL,
