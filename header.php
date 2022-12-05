25f70affc8f84f410b20da08af36a8d62afbdbb2
ArmelleThim0the <108655954+ArmelleThim0the@users.noreply.github.com>
2022-12-05 15:58:29 +0100

    header

--- header.php
+++ header.php
@@ -6,12 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
 
-<div class="bandeau"></div>
+<div class="bandeau">
+    <div id="logo">
+        <img src="./assets/images/logo_forum_vectoriel.png" alt="logo">
+    </div>
+    <div class="nom_forum">
+        <h1>Le forum des dev'</h1>
+    </div>
+    
+
+</div>
     
 </body>
 
