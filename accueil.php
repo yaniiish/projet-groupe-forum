863dead0f4805f2477ffe2b54c6673e9759e2d97
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2022-12-06 10:44:33 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
     <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="footer.css">
 </head>
 
 <body>
