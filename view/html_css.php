5b3ddab04d2a25e08102e35f4f8e63954597b37f
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2022-12-09 16:02:40 +0100

    topic

--- view/html_css.php
+++ view/html_css.php
@@ -9,6 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sujet: HTML / CSS</title>
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/generique.css">
+    <link rel="stylesheet" href="../assets/css/topic.css">
 </head>
 
 <body>
@@ -18,6 +20,24 @@
         <?php
         include('../composant/header.php');
         ?>
+    </div>
+
+    <div id="main">
+        <h1>Todo list</h1>
+        <div id="addBtn" onclick="openCreateDialog()">+</div>
+        <div id="list"></div>
+    </div>
+
+    <!-- fenêtre de création de nouveau todo -->
+    <div class="modal" id="modal1">
+        <div style="width:100%;text-align:right"><span class="closeBtn" onclick="closeCreateDialog()">X</span></div>
+        <h2>-- create new Todo --</h2>
+        <p class="pseudoTxtarea" id="content" contenteditable>&nbsp;</p>
+        <div style="text-align:center;"><span class="pseudoBtn" onclick="createTodo()">Créer</span></div>
+    </div>
+
+    <!-- fenêtre de mise à jour d'un todo existant -->
+    <div class="modal" id="modal2">
     </div>
 
 <div class="new">
