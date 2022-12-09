6ab0474e12791f00c70cffa92b46784a899aaf42
samantha-teixeira <samerika69@gmail.com>
2022-12-09 11:35:49 +0100

    style footer

--- view/inscription.php
+++ view/inscription.php
@@ -1,44 +1,50 @@
-<?php
-
-include("../composant/header.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/formulaire.css">
     <title>Inscription</title>
+    <link rel="stylesheet" href="../assets/css/formulaire.css">
+    <link rel="stylesheet" href="../assets/css/generique.css">
 </head>
 <body>
 
-<div class="formulaire">
-    <h1>S'inscrire</h1>
-        <form method="POST" action="inscription.php">
-            <div>
-                <input type="text"  class="input-text" placeholder="Votre pseudo" name="pseudo">
-            </div>
-            <div>
-                <input type="email" class="input-text" placeholder="Votre adresse e-mail" name="mail">
-            </div>
-            <div>
-                <input type="password" class="input-text" placeholder="Votre mot de passe" name="password">
-            </div>
-            <div>
-                <input type="password" class="input-text" placeholder="Confirmez votre mot de passe" name="confirm_password">
-            </div>
-            <div class="check">
+<div class="wrap">
+    <div class="header">
+        <?php include("../composant/header.php");?>
+    </div>
+
+    <div class="main">
+        <div class="formulaire">
+            <h1>S'inscrire</h1>
+            <form method="POST" action="inscription.php">
+                <div>
+                    <input type="text"  class="input-text" placeholder="Votre pseudo" name="pseudo">
+                </div>
+                <div>
+                    <input type="email" class="input-text" placeholder="Votre adresse e-mail" name="mail">
+                </div>
+                <div>
+                    <input type="password" class="input-text" placeholder="Votre mot de passe" name="password">
+                </div>
+                <div>
+                    <input type="password" class="input-text" placeholder="Confirmez votre mot de passe" name="confirm_password">
+                </div>
+                <div class="check">
                     <input class="checkbox" type="checkbox" name="check_inscription">
                     <p>Je suis d'accord avec les conditions générales d'utilisation et politique de confidentialité.</p>
-              
-            </div>
-            <div class="btn">
-                <input type="submit" name="submit" value="Créer mon compte">
-            </div>
-        </form>
+                </div>
+                <div class="btn">
+                    <input type="submit" name="submit" value="Créer mon compte">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="footer">
+        <?php include("../composant/footer.php");?>
+    </div>
 </div>
    
 </body>
@@ -57,11 +63,5 @@ if(!empty($_POST["submit"]) && !empty($_POST["pseudo"]) && !empty($_POST["mail"]
         "mdp" => $hash,
     ]);     
     header('Location:login.php');
-} 
-?>
-
-<?php
-
-include("../composant/footer.php");
-
+}
 ?>
--- view/login.php
+++ view/login.php
@@ -6,52 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Se connecter</title>
     <link rel="stylesheet" href="../assets/css/formulaire.css">
+    <link rel="stylesheet" href="../assets/css/generique.css">
 </head>
+
 <body>
+<div class="wrap">
 
- 
-<?php include '../composant/header.php'; ?>
+    <div class="header">
+        <?php include '../composant/header.php'; ?>
+    </div>
 
-<div class="formulaire">
-    <h1>Se connecter</h1>
-        <form name="form" method="POST" action="login.php">
-            <div>
-                <input type="text"  class="input-text" placeholder="Votre pseudo" name="pseudo">
-            </div>
-            <div>
-                <input type="password" class="input-text" placeholder="Votre mot de passe" name="password">
-            </div>
+    <div class="main">
+        <div class="formulaire">
+            <h1>Se connecter</h1>
+            <form name="form" method="POST" action="login.php">
+                <div>
+                    <input type="text"  class="input-text" placeholder="Votre pseudo" name="pseudo">
+                </div>
+                <div>
+                    <input type="password" class="input-text" placeholder="Votre mot de passe" name="password">
+                </div>
 
-<?php
-    require_once("../bdd/connexion_bdd.php");
-    
-    if (!empty($_POST['valider']) && !empty($_POST['pseudo']) && !empty($_POST['password'])) {
-        $pseudo = $_POST["pseudo"];
-        $password = $_POST["password"];
-        $requser=$pdo->prepare("SELECT*FROM utilisateurs WHERE pseudo=?");
-        $requser->execute([$pseudo]);
-        $userexist=$requser->fetch(PDO::FETCH_OBJ);
+                <?php
+                    require_once("../bdd/connexion_bdd.php");
+                        
+                    if (!empty($_POST['valider']) && !empty($_POST['pseudo']) && !empty($_POST['password'])) {
+                        $pseudo = $_POST["pseudo"];
+                        $password = $_POST["password"];
+                        $requser=$pdo->prepare("SELECT*FROM utilisateurs WHERE pseudo=?");
+                        $requser->execute([$pseudo]);
+                        $userexist=$requser->fetch(PDO::FETCH_OBJ);
 
-            if($userexist && password_verify($password, $userexist->mdp)) {
-                $userinfo=$requser->fetch();
-                $_SESSION['id']=$userinfo['pseudo'];
-                session_start();
-                $_SESSION['pseudo'] = $_POST['pseudo'];
-                header("location:index.php");
-            } else{
-                echo "<p style='color:red'>Mauvais login ou mot de passe!</p>";
-        }  
-    }
-    
-?>
+                        if($userexist && password_verify($password, $userexist->mdp)) {
+                            $userinfo=$requser->fetch();
+                            $_SESSION['id']=$userinfo['pseudo'];
+                            session_start();
+                            $_SESSION['pseudo'] = $_POST['pseudo'];
+                            header("location:index.php");
+                        } else{
+                            echo "<p style='color:red'>Mauvais login ou mot de passe!</p>";
+                        }  
+                    }       
+                ?>
+                <div class="btn">
+                    <input type="submit" name="valider" value="valider">
+                </div>
+            </form>
+        </div>
+    </div>
 
-            <div class="btn">
-                <input type="submit" name="valider" value="valider">
-            </div>
-        </form>
+    <div class="footer">
+        <?php
+        include('../composant/footer.php');
+        ?>
+    </div>
+
 </div>
-
-<?php include '../composant/footer.php'; ?>
 
 </body>
 </html>
